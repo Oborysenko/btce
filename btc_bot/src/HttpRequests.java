2f158100e8e8d50215a656f7286570e33826ecad
Oborysenko <borisenko.alexandr@gmail.com>
2014-01-12 21:59:42 +0200

    q

--- btc_bot/src/HttpRequests.java
+++ btc_bot/src/HttpRequests.java
@@ -1,5 +1,6 @@
 
 public class HttpRequests {
+	//TODO Unfinished method sendPost(String curPair)
 	public String sendPost(String curPair) {
 		return "";
 	}
--- btc_bot/src/Parser.java
+++ btc_bot/src/Parser.java
@@ -1,5 +1,6 @@
 
 public class Parser {
+	//TODO Unfinished method getValue(String response,String valueName)
 	public static String getValue(String response,String valueName) {
 		return "";
 	}
