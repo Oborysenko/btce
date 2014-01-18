5c9846f0cb73cc7252561c6885203607ac0f4737
Oborysenko <alexandr.borisenko@gmail.com>
2014-01-19 01:26:15 +0200

    q

--- btc_bot/src/VirtualMoney.java
+++ btc_bot/src/VirtualMoney.java
@@ -3,5 +3,5 @@ public class VirtualMoney {
 	public static double USD = 0;
 	public static double BTC = 10;
 	public static double lastPrice = 0;
-	public static boolean isBuyed = false;
+	public static boolean isBuyed = true;
 }
